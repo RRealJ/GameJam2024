24f0b611a915682c0537f3170228827b253e7b2c
RRealJ <renatorealjunior2@gmail.com>
2024-10-18 22:31:58 -0300

    fix

--- scripts/peca_geral.gd
+++ scripts/peca_geral.gd
@@ -2,8 +2,9 @@ extends Node2D
 
 class_name Peca
 
-@onready var area = $textura/area
+@onready var area = $sprite/area
 @onready var textura : Resource #vai pegar a textura do estagio atraves de load/preload
+
 
 func _on_ready() -> void:
 	$sprite.texture = textura #atribui a textura que quiser ao carregar na cena
