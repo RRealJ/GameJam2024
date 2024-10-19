c342627909be04e66c980aff37fcdb8e17b76e79
RRealJ <renatorealjunior2@gmail.com>
2024-10-18 21:37:26 -0300

    script peca_geral, para base de todas as pecas

--- scripts/peca_geral.gd
+++ scripts/peca_geral.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+class_name Peca
+
+@onready var area = $textura/area
+@onready var textura : Resource
+
+func _on_ready() -> void:
+	$sprite.texture = textura #atribui a textura que quiser ao carregar na cena
