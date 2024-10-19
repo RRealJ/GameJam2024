8e73944f9649e703bbb99557e91841200d6fb558
RRealJ <renatorealjunior2@gmail.com>
2024-10-19 00:44:12 -0300

    adicionado peça L e peça T

--- scripts/peca_geral.gd
+++ scripts/peca_geral.gd
@@ -4,18 +4,18 @@ class_name Peca
 
 @onready var area2d = $sprite/area
 @onready var textura : Resource #vai pegar a textura do estagio atraves de load/preload
-@onready var sprite = $sprite.texture
+@onready var sprite = $sprite
 
 
 func _on_ready() -> void:
-	$sprite.texture = textura #atribui a textura que quiser ao carregar na cena
+	sprite.texture = textura #atribui a textura que quiser ao carregar na cena
 
 
-func _on_area_entered(area) -> void: #quando entrar em area peca, fica vermelho
+func _on_area_entered(area) -> void: #quando entrar em area peça, fica vermelha
 	if area is Peca:
 		sprite.modulate = Color8(186, 8, 0, 45)
 		
 
-func _on_area_exited(area) -> void: #quando sair de area peca, fica normal
+func _on_area_exited(area) -> void: #quando sair de area peça, fica normal
 	if area is Peca:
 		sprite.modulate = Color8(186, 8, 0, 0)
