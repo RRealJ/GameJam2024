a2018aeb566b6211dc1417697d71843fa2fa96e8
RRealJ <renatorealjunior2@gmail.com>
2024-10-18 23:11:39 -0300

    arrumando peca2x2, modulacao

--- scripts/peca_geral.gd
+++ scripts/peca_geral.gd
@@ -2,9 +2,20 @@ extends Node2D
 
 class_name Peca
 
-@onready var area = $sprite/area
+@onready var area2d = $sprite/area
 @onready var textura : Resource #vai pegar a textura do estagio atraves de load/preload
+@onready var sprite = $sprite.texture
 
 
 func _on_ready() -> void:
 	$sprite.texture = textura #atribui a textura que quiser ao carregar na cena
+
+
+func _on_area_entered(area) -> void: #quando entrar em area peca, fica vermelho
+	if area is Peca:
+		sprite.modulate = Color8(186, 8, 0, 45)
+		
+
+func _on_area_exited(area) -> void: #quando sair de area peca, fica normal
+	if area is Peca:
+		sprite.modulate = Color8(186, 8, 0, 0)
