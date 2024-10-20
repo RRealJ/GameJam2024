a2461678d812e7c7f247b9a6b481f59902e50666
RRealJ <renatorealjunior2@gmail.com>
2024-10-19 22:14:59 -0300

    menu principal adicionado

--- scripts/estagios.gd
+++ scripts/estagios.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 func set_pecas_index_to_1():  #Deixa todas as peças no z_index = 1
 	var pecas = self.get_children()
-	for p in pecas:
+	for p in pecas: 
 		if p is Peca:
 			p.z_index = 1
+			print("z_index mudado para 1")
---
+++ scripts/menu_principal.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+	
+
+
+func _on_btn_sair_pressed() -> void:
+	get_tree().quit()
