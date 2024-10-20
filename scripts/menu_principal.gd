1ba56731cb72db6d4555351c537b361713dd62ef
RRealJ <renatorealjunior2@gmail.com>
2024-10-19 22:54:57 -0300

    colocado mais resolucoes

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -10,6 +10,15 @@ func _process(delta: float) -> void:
 	pass
 	
 
-
 func _on_btn_sair_pressed() -> void:
 	get_tree().quit()
+	
+	
+func _unhandled_input(event):
+	if event.is_action_pressed("f11"):
+		if Global.fullscreen == true:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			Global.fullscreen = false
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			Global.fullscreen = true
